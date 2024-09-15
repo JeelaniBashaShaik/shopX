78f9d6f4d55d6b5f58f8cbc6d302634fd0570506
Jeelani Shaik <jeelanishaik@Jeelanis-MacBook-Air.local>
2024-09-16 00:23:42 +0530

    enhance home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,16 +1,42 @@
 import { Component, inject } from '@angular/core';
 import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
+import {MatIconModule} from '@angular/material/icon';
 import {Subject} from 'rxjs';
 import {takeUntil} from 'rxjs/operators';
+import { HomeTileComponent } from '../home-tile/home-tile.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule, HomeTileComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+
+
+  homeTiles = [
+    {
+      imagePath: '../assets/images/laser_house.jpeg',
+      title: 'DIY toy houses'
+    },
+    {
+      imagePath: '../assets/images/laser_art.jpg',
+      title: 'Wall art',
+    },
+    {
+      imagePath: '../assets/images/laser_maze.avif',
+      title: 'Mazes'
+    },
+    {
+      imagePath: '../assets/images/laser_box.jpg',
+      title: 'Storage boxes'
+    },
+    {
+      imagePath: '../assets/images/laser_lamp.jpg',
+      title: 'Lamps'
+    },
+  ]
 
   breakpointObserver = inject(BreakpointObserver)
   ngOnInit() {
